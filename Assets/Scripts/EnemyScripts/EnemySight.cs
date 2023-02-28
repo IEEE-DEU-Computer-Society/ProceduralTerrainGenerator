90c2358ac05a872bfffc336d905e4f503f54fb0b
Özgür Dalbeler <ozgurdalbeler@gmail.com>
2023-02-28 19:58:29 +0300

    commit
    commit

---
+++ Assets/Scripts/EnemyScripts/EnemySight.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class EnemySight : MonoBehaviour
+{
+    //assign
+    public EnemyState state;
+    public Rigidbody2D rb;
+
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/EnemyScripts/EnemyState.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyState : MonoBehaviour
+{
+    public bool isMoving;
+    public bool isAttacking;
+}
