6b28e45564133257c269e08decc413b95a3ef7ca
L4qqer <firatege52@gmail.com>
2023-03-13 12:11:42 +0300

    New Homing Missle Turret
    maybe the tower is finished xd

--- Assets/Scripts/TuretScripts/FireRotationScript.cs
+++ Assets/Scripts/TuretScripts/FireRotationScript.cs
@@ -12,18 +12,16 @@ public class FireRotationScript : MonoBehaviour
 
     public float force = 8;
 
-    private float xdegis, ydegis;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        Controlled();
+        
         //fire
-        Vector3 direction = enemy.transform.position - transform.position;
-        rgb.velocity = new Vector2(direction.x + xdegis, direction.y + ydegis).normalized * force;
+       
 
-        float rot = Mathf.Atan2(-direction.y, -direction.x) * Mathf.Rad2Deg;
-        transform.rotation = Quaternion.Euler(0, 0, rot + 90);
+        
 
     }
 
@@ -52,30 +50,16 @@ public class FireRotationScript : MonoBehaviour
     //Enemy Hit
     IEnumerator Hit()
     {
-        enemy.GetComponent<Rigidbody2D>().velocity = new Vector2(enemy.transform.position.x - transform.position.x,
-            enemy.transform.position.y - transform.position.y);
+        enemy.GetComponent<Rigidbody2D>().velocity = new Vector2(enemy.transform.position.x - transform.position.x ,
+            enemy.transform.position.y - transform.position.y );
         yield return new WaitForSeconds(0.2f);
         enemy.GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
+        Destroy(this.gameObject);
 
 
     }
 
-    private void Controlled()
-    {
-
-        if (transform.position.y - enemy.transform.position.y > 2.5f ||
-            transform.position.y - enemy.transform.position.y < -2.5f ||
-            transform.position.x - enemy.transform.position.x > 0.8)
-        {
-            Debug.Log("HEHEAHEHEHEAHEAHE");
-            xdegis = -1.5f;
-        }
-        else
-        {
-            xdegis = 0f;
-            Debug.Log("ZAZAZAZAZAZAZAZAZ");
-        }
-    }
+    
 
 }
     
