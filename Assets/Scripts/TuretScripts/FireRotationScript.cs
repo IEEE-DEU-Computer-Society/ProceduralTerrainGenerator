6f391bc51ea5710a2debb7bd3adb493707e8f304
L4qqer <firatege52@gmail.com>
2023-03-13 11:25:40 +0300

    Bullet (Eski sistem)
    Not use NavMash

--- Assets/Scripts/TuretScripts/FireRotationScript.cs
+++ Assets/Scripts/TuretScripts/FireRotationScript.cs
@@ -11,44 +11,72 @@ public class FireRotationScript : MonoBehaviour
     public Rigidbody2D rgb;
 
     public float force = 8;
+
+    private float xdegis, ydegis;
+
     // Start is called before the first frame update
     void Start()
     {
-        
-      
+        Controlled();
+        //fire
         Vector3 direction = enemy.transform.position - transform.position;
-        rgb.velocity = new Vector2(direction.x, direction.y).normalized * force;
+        rgb.velocity = new Vector2(direction.x + xdegis, direction.y + ydegis).normalized * force;
 
         float rot = Mathf.Atan2(-direction.y, -direction.x) * Mathf.Rad2Deg;
-        transform.rotation = Quaternion.Euler(0,0,rot+90);
+        transform.rotation = Quaternion.Euler(0, 0, rot + 90);
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+
     }
 
+    //InVisable Bullet
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.tag == "enemy")
         {
 
-            
+
             StartCoroutine(Hit());
             this.GetComponent<SpriteRenderer>().enabled = false;
 
 
         }
-        
+
     }
 
+    //Enemy Hit
     IEnumerator Hit()
     {
-        enemy.GetComponent<Rigidbody2D>().velocity = new Vector2(enemy.transform.position.x-transform.position.x,enemy.transform.position.y-transform.position.y);
+        enemy.GetComponent<Rigidbody2D>().velocity = new Vector2(enemy.transform.position.x - transform.position.x,
+            enemy.transform.position.y - transform.position.y);
         yield return new WaitForSeconds(0.2f);
         enemy.GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
 
 
     }
+
+    private void Controlled()
+    {
+
+        if (transform.position.y - enemy.transform.position.y > 2.5f ||
+            transform.position.y - enemy.transform.position.y < -2.5f ||
+            transform.position.x - enemy.transform.position.x > 0.8)
+        {
+            Debug.Log("HEHEAHEHEHEAHEAHE");
+            xdegis = -1.5f;
+        }
+        else
+        {
+            xdegis = 0f;
+            Debug.Log("ZAZAZAZAZAZAZAZAZ");
+        }
+    }
+
 }
+    
+
